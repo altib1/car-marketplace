e82a84ba5e2bb5ada5937e91e761dffb842bb162
altib1 <altiballa@gmail.com>
2024-10-11 10:51:43 +0200

    Profile and Publications

--- src/src/Controller/ProfileController.php
+++ src/src/Controller/ProfileController.php
@@ -5,15 +5,36 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Publication;
+use App\Form\PublicationType;
+use App\Repository\PublicationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, PublicationRepository $publicationRepository): Response
     {
         // usually you'll want to make sure the user is authenticated first,
         // see "Authorization" below
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $publication = new Publication();
+        $publication->setUser($this->getUser());
+
+        $form = $this->createForm(PublicationType::class, $publication);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($publication);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        $publications = $publicationRepository->findBy(['user' => $this->getUser()]);
 
         // returns your User object, or null if the user is not authenticated
         // use inline documentation to tell your editor your exact User class
@@ -24,6 +45,8 @@ class ProfileController extends AbstractController
         // For example, if you added a getFirstName() method, you can use that.
         return $this->render('profile/index.html.twig', [
             'user' => $user,
+            'publicationForm' => $form->createView(),
+            'publications' => $publications,
         ]);
     }
 }
---
+++ src/src/Form/PublicationType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CarBrand;
+use App\Entity\Publication;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PublicationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('price')
+            ->add('year')
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('brand', EntityType::class, [
+                'class' => CarBrand::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Publication::class,
+        ]);
+    }
+}
