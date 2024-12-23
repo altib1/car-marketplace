01433751350a7de239ae1612df91ceb69964a20a
altib1 <altiballa@gmail.com>
2024-12-24 00:45:24 +0100

    Adding a functionning v1 template

--- src/src/Service/PublicationSearchService.php
+++ src/src/Service/PublicationSearchService.php
@@ -8,6 +8,7 @@ use Elastica\Query\BoolQuery;
 use Elastica\Query\MultiMatch;
 use Elastica\Query\Range;
 use Elastica\Query\Term;
+use Elastica\Query\Nested;
 
 class PublicationSearchService
 {
@@ -18,7 +19,7 @@ class PublicationSearchService
     public function search(array $criteria)
     {
         $boolQuery = new BoolQuery();
-
+    
         // Search term (searches in title and description)
         if (!empty($criteria['q'])) {
             $multiMatch = new MultiMatch();
@@ -26,7 +27,7 @@ class PublicationSearchService
             $multiMatch->setQuery($criteria['q']);
             $boolQuery->addMust($multiMatch);
         }
-
+    
         // Price range
         if (!empty($criteria['price_min']) || !empty($criteria['price_max'])) {
             $rangeQuery = new Range();
@@ -40,7 +41,7 @@ class PublicationSearchService
             $rangeQuery->addField('price', $range);
             $boolQuery->addFilter($rangeQuery);
         }
-
+    
         // Year range
         if (!empty($criteria['year_min']) || !empty($criteria['year_max'])) {
             $rangeQuery = new Range();
@@ -54,27 +55,36 @@ class PublicationSearchService
             $rangeQuery->addField('year', $range);
             $boolQuery->addFilter($rangeQuery);
         }
-
+    
         // Brand filter
         if (!empty($criteria['brand'])) {
+            $nestedQuery = new Nested();
+            $nestedQuery->setPath('brand');
             $termQuery = new Term(['brand.id' => $criteria['brand']]);
-            $boolQuery->addFilter($termQuery);
+            $nestedQuery->setQuery($termQuery);
+            $boolQuery->addFilter($nestedQuery);
         }
-
+    
         // Model filter
         if (!empty($criteria['model'])) {
+            $nestedQuery = new Nested();
+            $nestedQuery->setPath('model');
             $termQuery = new Term(['model.id' => $criteria['model']]);
-            $boolQuery->addFilter($termQuery);
+            $nestedQuery->setQuery($termQuery);
+            $boolQuery->addFilter($nestedQuery);
         }
-
+    
         // Motorization type filter
         if (!empty($criteria['motorization_type'])) {
+            $nestedQuery = new Nested();
+            $nestedQuery->setPath('motorizationType');
             $termQuery = new Term(['motorizationType.id' => $criteria['motorization_type']]);
-            $boolQuery->addFilter($termQuery);
+            $nestedQuery->setQuery($termQuery);
+            $boolQuery->addFilter($nestedQuery);
         }
-
+    
         $query = new Query($boolQuery);
-
+    
         // Add sorting
         if (!empty($criteria['sort'])) {
             switch ($criteria['sort']) {
@@ -89,11 +99,26 @@ class PublicationSearchService
                     break;
             }
         }
-
+    
         // Pagination
         $page = $criteria['page'] ?? 1;
         $perPage = $criteria['per_page'] ?? 10;
-        
-        return $this->publicationsFinder->findPaginated($query);
+    
+        // Get the results from the finder
+        $paginatedResults = $this->publicationsFinder->findPaginated($query);
+        $paginatedResults->setCurrentPage($page);
+        $paginatedResults->setMaxPerPage($perPage);
+    
+        // Access the current page results
+        return [
+            'results' => $paginatedResults->getCurrentPageResults(),
+            'pagination' => [
+                'current_page' => $page,
+                'total_pages' => ceil($paginatedResults->getNbResults() / $perPage),
+                'previous_page' => $page > 1 ? $page - 1 : null,
+                'next_page' => $page < ceil($paginatedResults->getNbResults() / $perPage) ? $page + 1 : null,
+            ]
+        ];
     }
+    
 }
