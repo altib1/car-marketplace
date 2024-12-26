1f8508a1ff44e59b0c8dbe535ad1af43d6055d09
altib1 <altiballa@gmail.com>
2024-12-26 20:02:42 +0100

    Adding missing code

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,3 +1,16 @@
+// assets/app.js
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const brandSelect = document.getElementById('publication_brand');
     const modelSelect = document.getElementById('publication_model');
