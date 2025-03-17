b447932d170c904b3108240fd80969466663eb8f
altib1 <altiballa@gmail.com>
2025-03-17 23:47:31 +0100

    Adding corrections

--- src/src/Form/ShopType.php
+++ src/src/Form/ShopType.php
@@ -17,6 +17,8 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class ShopType extends AbstractType
 {
@@ -73,6 +75,19 @@ class ShopType extends AbstractType
                 'label' => 'Creation Date',
                 'required' => true,
                 'widget' => 'single_text',
+                'html5' => true,
+                'empty_data' => new \DateTime(),
+                'attr' => [
+                    'class' => 'datepicker',
+                    'required' => 'required',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a creation date',
+                    ]),
+                ],
+                'invalid_message' => 'Please enter a valid date.',
+                'error_bubbling' => false,
             ])
             ->add('address', TextType::class, [
                 'label' => 'Address',
@@ -90,6 +105,16 @@ class ShopType extends AbstractType
                 'label' => 'Work Hours',
                 'required' => true,
             ]);
+
+
+
+            $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+                $data = $event->getData();
+                if (empty($data['creationDate'])) {
+                    $data['creationDate'] = date('Y-m-d'); // Today's date
+                    $event->setData($data);
+                }
+            });
     }
 
     public function validateUniqueName($value, ExecutionContextInterface $context)
